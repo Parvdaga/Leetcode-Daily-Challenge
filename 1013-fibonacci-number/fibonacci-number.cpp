ec7b2ff7f85ce6626eb3124a8b31e143199a10b2
Parv Daga <123968142+Parvdaga@users.noreply.github.com>
2025-08-25 11:16:08 +0530

    Time: 3 ms (66.12%) | Memory: 7.7 MB (87.26%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -3,14 +3,17 @@ public:
     int fib(int n) {
         if(n <= 1) return n;
         
-        vector<int> dp(n+1, -1);
-        dp[0] = 0;
-        dp[1] = 1;
-        
+        // vector<int> dp(n+1, -1);
+        // dp[0] = 0;
+        // dp[1] = 1;
+        int p2=0, p1=1;
         for(int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            // dp[i] = dp[i-1] + dp[i-2];
+            int cur = p1+p2;
+            p2=p1;
+            p1=cur;
         }
         
-        return dp[n];
+        return p1;
     }
 };
